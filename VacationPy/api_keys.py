c366e497399474013bfc003502b01c67a5907948
TEMIDAYO AKINSANYA <tiakinsa@us.ibm.com>
2020-08-27 20:23:26 -0400

    Code for weatherpy is completed without written analysis

---
+++ VacationPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "19ba061dfc3b63f37354f4cc5e831bc5"
+
+# Google API Key
+g_key = "AIzaSyDLwIYt6w1w8i9CUvEOcs1qNgh16BYoW1Y"
---
+++ WeatherPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "579df1695b4e81f1856ea4f537062582"
+
+# Google API Key
+g_key = "AIzaSyDLwIYt6w1w8i9CUvEOcs1qNgh16BYoW1Y"
